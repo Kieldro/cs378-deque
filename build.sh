fca32bec6a66cd838145b12728509ac06bd0ffce
Ian Buitrago <kieldro@gmail.com>
2012-07-05 22:37:07 -0500

    First commit.

---
+++ build.sh
@@ -0,0 +1,27 @@
+# file variables
+source="Deque.c++"
+unitFile="TestDeque.c++"
+outFile="TestDeque.out"
+
+clear
+echo COMPILING $source...
+g++ -ansi -pedantic -Wall $source -o $source.app
+echo COMPILING $unitFile...
+g++ -ansi -pedantic -ldl -Wall $unitFile -lcppunit -o $unitFile.app
+	if ([ $? == 0 ]); then
+echo RUNNING UNIT TESTS...
+./$unitFile.app #>& $outFile
+	fi
+
+<<MULTICOMMENT
+echo CHECKING OUTPUT...; diff -lc RunInteger.out RunInteger.in
+
+echo GENERATING COMMIT LOG...
+git log > Deque.log
+
+#echo RUNNING DOXYGEN...; doxygen Doxyfile
+
+zip Deque README.txt html/* Deque.h Deque.log TestDeque.c++ TestDeque.out
+
+turnin --submit inbleric cs378pj4 Deque.zip
+MULTICOMMENT
