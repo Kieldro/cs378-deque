49a3d74d3a7c3614d6c092561901f7c123a0f51b
Ian Buitrago <kieldro@gmail.com>
2012-07-11 18:02:59 -0500

    added more required files.

--- build.sh
+++ build.sh
@@ -8,18 +8,24 @@ echo COMPILING $source and $unitFile...
 g++ -ansi -pedantic -ldl -Wall $unitFile -lcppunit -o $unitFile.app
 	if ([ $? == 0 ]); then
 echo RUNNING UNIT TESTS...
-./$unitFile.app #>& $outFile
+valgrind ./$unitFile.app #>& $outFile
 	fi
 
 <<MULTICOMMENT
-echo CHECKING OUTPUT...; diff -lc RunInteger.out RunInteger.in
-
 echo GENERATING COMMIT LOG...
 git log > Deque.log
 
-#echo RUNNING DOXYGEN...; doxygen Doxyfile
+echo RUNNING DOXYGEN...
+# doxygen -g
+# EXTRACT_ALL            = YES
+# EXTRACT_PRIVATE        = YES
+# EXTRACT_STATIC         = YES
+# GENERATE_LATEX         = NO
+doxygen Doxyfile
 
 zip Deque README.txt html/* Deque.h Deque.log TestDeque.c++ TestDeque.out
 
-turnin --submit inbleric cs378pj4 Deque.zip
+turnin --submit	inbleric cs378pj4 Deque.zip
+turnin --list	inbleric cs378pj4
+turnin --verify	inbleric cs378pj4
 MULTICOMMENT
