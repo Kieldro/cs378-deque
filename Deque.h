a59d64bd30dc5182c009ee7b9c9725668fde3875
fin <fin@cs.utexas.edu>
2012-07-15 11:09:15 -0500

    whitespace

--- Deque.h
+++ Deque.h
@@ -73,17 +73,17 @@ class MyDeque {
 	public:
 		// --------
 		// typedefs
-		typedef A							allocator_type;
+		typedef A											allocator_type;
 		typedef typename allocator_type::value_type			value_type;		
 
 		typedef typename allocator_type::size_type			size_type;
-		typedef typename allocator_type::difference_type		difference_type;
+		typedef typename allocator_type::difference_type	difference_type;
 
 		typedef typename allocator_type::pointer			pointer;
-		typedef typename allocator_type::const_pointer			const_pointer;
+		typedef typename allocator_type::const_pointer		const_pointer;
 
 		typedef typename allocator_type::reference			reference;
-		typedef typename allocator_type::const_reference		const_reference;
+		typedef typename allocator_type::const_reference	const_reference;
 		
 
 	public:
